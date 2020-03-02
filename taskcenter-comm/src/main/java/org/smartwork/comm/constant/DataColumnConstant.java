7741a44d3909eac2e6dff1b67b960f1b0af8137e
cfmkaifa <cfmkaifa@shdbr.com>
2020-03-02 11:52:14 +0800

    comm

---
+++ taskcenter-comm/src/main/java/org/smartwork/comm/constant/DataColumnConstant.java
@@ -0,0 +1,8 @@
+package src.main.java.org.smartwork.comm.constant;
+
+public interface DataColumnConstant {
+
+
+	/*******任务竞标记录相关*******/
+	public static final String USERNAME = "username";
+}
---
+++ taskcenter-comm/src/main/java/org/smartwork/comm/model/ZGTaskAttachDto.java
@@ -0,0 +1,58 @@
+package src.main.java.org.smartwork.comm.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/***
+ * 类概述:任务附件dto
+ * @创建人 niehy(Frunk)
+ * @创建时间 2020/2/29
+ * @修改人 (修改了该文件，请填上修改人的名字)
+ * @修改日期 (请填上修改该文件时的日期)
+ */
+@Data
+public class ZGTaskAttachDto implements Serializable{
+
+    private static final long serialVersionUID = 3591264373289084697L;
+    /**
+     * 任务ID
+     *
+     * Table:     fb_zg_task_attach
+     * Column:    task_id
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "任务ID",example="0")
+    private Long taskId;
+
+    /**
+     * 中文名
+     *
+     * Table:     fb_zg_task_attach
+     * Column:    cn_name
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "中文名",example="")
+    private String cnName;
+
+    /**
+     * 后缀名
+     *
+     * Table:     fb_zg_task_attach
+     * Column:    suffix
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "后缀名",example="")
+    private String suffix;
+
+    /**
+     * 文件路径
+     *
+     * Table:     fb_zg_task_attach
+     * Column:    file_path
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "文件路径",example="")
+    private String filePath;
+}
