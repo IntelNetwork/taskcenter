5d979737a634c82f295d8b9d071466ac51022b28
cfmkaifa <cfmkaifa@shdbr.com>
2020-03-03 15:07:27 +0800

    方法名修改(Tom)

--- taskcenter-biz/src/main/java/org/smartwork/biz/service/impl/ZGTaskServiceImpl.java
+++ taskcenter-biz/src/main/java/org/smartwork/biz/service/impl/ZGTaskServiceImpl.java
@@ -6,6 +6,8 @@ import org.forbes.comm.constant.DataColumnConstant;
 import org.forbes.comm.utils.ConvertUtils;
 import org.forbes.comm.vo.Result;
 import org.smartwork.biz.service.IZGTaskService;
+import org.smartwork.comm.constant.CommonConstant;
+import org.smartwork.comm.constant.TaskAttachColumnConstant;
 import org.smartwork.comm.constant.TaskColumnConstant;
 import org.smartwork.dal.entity.ZGTask;
 import org.smartwork.dal.entity.ZGTaskAttach;
@@ -19,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.smartwork.comm.model.ZGTaskAttachDto;
 import org.smartwork.comm.model.ZGTaskDto;
 
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -51,7 +55,7 @@ public class ZGTaskServiceImpl extends ServiceImpl<ZGTaskMapper, ZGTask> impleme
         if (ConvertUtils.isNotEmpty(zgTaskAttachDtos)) {
             Long taskId = task.getId();
             ZGTaskAttach attach = new ZGTaskAttach();
-            zgTaskAttachDtos.stream().forEach(temp-> {
+            zgTaskAttachDtos.stream().forEach(temp -> {
                 attach.setId(taskId);
                 attach.setCnName(temp.getCnName());
                 attach.setFilePath(temp.getFilePath());
@@ -62,6 +66,46 @@ public class ZGTaskServiceImpl extends ServiceImpl<ZGTaskMapper, ZGTask> impleme
         return null;
     }
 
+
+    /***
+     * removeTask方法概述:删除任务,关联附件
+     * @param id
+     * @return
+     * @创建人 niehy(Frunk)
+     * @创建时间 2020/3/3
+     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改日期 (请填上修改该文件时的日期)
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void removeTask(String id) {
+        baseMapper.deleteById(id);
+        //同时删除任务附件
+        zgTaskAttachMapper.delete(new QueryWrapper<ZGTaskAttach>().eq(TaskAttachColumnConstant.TASK_ID, id));
+    }
+
+
+    /***
+     * removeTasks方法概述:批量删除任务,关联附件
+     * @param ids
+     * @return
+     * @创建人 niehy(Frunk)
+     * @创建时间 2020/3/3
+     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改日期 (请填上修改该文件时的日期)
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void removeTasks(String ids) {
+        List idList = Arrays.asList(ids.split(CommonConstant.SEPARATOR));
+        idList.stream().forEach(id -> {
+            baseMapper.deleteById((Serializable) id);
+            //删除所有附件
+            zgTaskAttachMapper.delete(new QueryWrapper<ZGTaskAttach>().eq(TaskAttachColumnConstant.TASK_ID, id));
+        });
+    }
+
+
     /***
      * 获取最新成交动态
      */
@@ -69,6 +113,7 @@ public class ZGTaskServiceImpl extends ServiceImpl<ZGTaskMapper, ZGTask> impleme
     public List<ZGTask> selectAllTask() {
         return zgTaskExtMapper.selectAllTask();
     }
+
 
     /***
      * updateTask方法概述:任务编辑
@@ -79,17 +124,17 @@ public class ZGTaskServiceImpl extends ServiceImpl<ZGTaskMapper, ZGTask> impleme
      * @修改人 (修改了该文件，请填上修改人的名字)
      * @修改日期 (请填上修改该文件时的日期)
      */
-    @Transactional(rollbackFor=Exception.class)
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void updateTask(ZGTaskDto zgTaskDto) {
-        ZGTask zgTask=new ZGTask();
-        BeanCopier.create(ZGTaskDto.class,ZGTask.class ,false).copy(zgTaskDto, zgTask, null);
+        ZGTask zgTask = new ZGTask();
+        BeanCopier.create(ZGTaskDto.class, ZGTask.class, false).copy(zgTaskDto, zgTask, null);
         baseMapper.updateById(zgTask);
         //删除 zgTaskAttachMapper
         zgTaskAttachMapper.delete(new QueryWrapper<ZGTaskAttach>().eq(TaskColumnConstant.ID, zgTaskDto.getId()));
         Long zgTaskId = zgTask.getId();
         List<ZGTaskAttachDto> zgTaskAttachDtos = zgTaskDto.getZgTaskAttachDtos();
-        if(ConvertUtils.isNotEmpty(zgTaskAttachDtos)){
+        if (ConvertUtils.isNotEmpty(zgTaskAttachDtos)) {
             zgTaskAttachDtos.stream().forEach(zgTaskAttachDto -> {
                 ZGTaskAttach zgTaskAttach = new ZGTaskAttach();
                 zgTaskAttach.setTaskId(zgTaskId);
